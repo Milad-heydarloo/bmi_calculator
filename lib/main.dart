99720fd10a8e2abd3e64b51d40965736ccfd28fb
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-07-17 12:09:39 -0700

    hintStyle: , TextField , TextStyle ,

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/constance/constance.dart';
 import 'package:bmi_calculator/widget/background_shape_left.dart';
 import 'package:bmi_calculator/widget/background_shape_right.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +14,29 @@ class run_app extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(title: Text('BMI')),
         body: SafeArea(
-          child: Column(children: [
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  //khat rahnama hazf
+                  border: InputBorder.none,
+                  hintText: 'وزن',
+
+                  hintStyle: TextStyle(color: greenback),
+                ),
+                style: TextStyle(
+                  color: greenback,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                //vorodi chi bashe
+
+                keyboardType: TextInputType.number,
+              ),
+            ),
             Right_shape(x: 124.0),
             Left_shape(x: 145.0),
           ]),
